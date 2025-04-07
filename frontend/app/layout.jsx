3c088383bd5adf5d5b1d2bda94127c21b27eca97
Sourabha K H <sourabhaprasad04@gmail.com>
2025-04-08 01:18:11 +0530

    meta data

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -5,6 +5,9 @@ import "./globals.css";
 export const metadata = {
   title: "Homestay Finder",
   description: "Comfort",
+  icons: {
+    icon: "logo/homestay1.png",
+  },
 };
 
 export default function RootLayout({ children }) {
