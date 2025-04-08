a44dd3d6e9fdb8ef88c58cc3ef8a3961dfe40912
Sourabha K H <sourabhaprasad04@gmail.com>
2025-04-09 01:02:15 +0530

    routing

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import propertyRoutes from "./routes/property.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -15,7 +16,9 @@ app.get("/", (req, res) => {
   res.send("Hotel Booking Management Backend");
 });
 
+// Routes
 app.use("/api/properties", propertyRoutes);
+app.use("/api/users", authRoutes);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () =>
