d0054cebf4aa38a585837ab25376ed13787999e5
Sourabha K H <sourabhaprasad04@gmail.com>
2025-04-09 02:37:57 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
