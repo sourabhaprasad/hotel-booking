7c53a0ad0ba10dba9afa24abaa6907e1c9f94c00
sourabhaprasad <sourabhaprasad04@gmail.com>
2025-06-15 22:38:11 +0530

    fix: UI change

--- frontend/app/components/FeatureCard.jsx
+++ frontend/app/components/FeatureCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const FeatureCard = ({ icon, title, description }) => {
   return (
-    <div className="bg-white/10 px-6 py-6 rounded-lg w-64 shadow-lg min-h-[200px] hover:bg-white/20 transition duration-300 ease-in-out">
+    <div className="bg-white/10 px-6 py-6 rounded-lg w-64 shadow-lg min-h-[280px] hover:bg-white/20 transition duration-300 ease-in-out">
       <div className="flex flex-col items-center">
         <div className="text-3xl mb-4">{icon}</div>
         <h3 className="text-white text-base font-bold mb-2 text-center">
